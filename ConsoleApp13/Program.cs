9e1001a80eb3c0781485df378ac3ef820c2d7c94
IvaFil <filipovichiv@yandex.ru>
2022-09-29 10:06:56 +0300

    Задача 47

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введи количество строк: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введи количество столбцов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double[,] arr = new double[m,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i,j] = Convert.ToDouble(new Random().NextDouble());
+
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(arr[i,j]+"   ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
